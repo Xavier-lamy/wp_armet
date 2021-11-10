56fd289fbe0109a179df463858811eb14013f5d3
Xavier-lamy <xavier.lamy.pro@gmail.com>
2021-11-10 16:03:59 +0100

    Add font awesome kit

--- wp-content/themes/armet/inc/functions-config.php
+++ wp-content/themes/armet/inc/functions-config.php
@@ -1,13 +1,16 @@
 <?php
 
-function armet_enqueue_scripts() {
+function armet_enqueue_assets() {
     // Load style.css
-    wp_enqueue_style( 'style', get_stylesheet_uri(), array(), 1.0 );
+    wp_enqueue_style( 'armet_style', get_stylesheet_uri(), array(), 1.0 );
+  
+    // Load CSS
+    wp_enqueue_style( 'armet_main', get_template_directory_uri() . '/assets/css/main.css', array(), 1.0 );
 
     // Load JS
-    wp_enqueue_script( 'script', get_template_directory_uri() . 'assets/js/script.js', array(), 1.0, true );
+    wp_enqueue_script( 'armet_script', get_template_directory_uri() . '/assets/js/script.js', array(), 1.0, true );
 
-    // Load CSS
-    wp_enqueue_style( 'main', get_template_directory_uri() . 'assets/css/main.css', array(), 1.0 );
+    // Load Font Awesome library
+    wp_enqueue_script( 'fa_kit', 'https://kit.fontawesome.com/0cb3a70ede.js', array(), 1.0, true );
 }
-add_action('wp_enqueue_script', 'armet_enqueue_scripts');
+add_action('wp_enqueue_scripts', 'armet_enqueue_assets');

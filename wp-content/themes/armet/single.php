a6e4c1e35e859f8a9f04ff14a588680adb84108f
Xavierlamy <xavier.lamy.pro@gmail.com>
2021-11-30 22:54:12 +0100

    Add last-news bloc under singles

--- wp-content/themes/armet/single.php
+++ wp-content/themes/armet/single.php
@@ -27,6 +27,41 @@
             </div>
             </article>
         <?php endwhile; endif; ?>
+            <div class="lasts-news">
+        <?php 
+            //3 last articles from latest to oldest
+            $args = array(
+                'post_type' => 'post',
+                'posts_per_page' => 3,
+                'order' => 'DESC',
+                'orderby' => 'date',
+            );
+
+            $my_query = new WP_Query( $args );
+
+            //Loop (similar to standard wp one)
+            if( $my_query->have_posts() ) : while( $my_query->have_posts() ) : $my_query->the_post(); ?>
+                
+            <div class="card">
+                <span class="tag"><?php echo the_tags(); ?></span>
+                <div class="card__img">
+                    <?php echo the_post_thumbnail('medium'); ?>    
+                </div>
+                <div class="card__body">
+                    <h3><?php echo the_title(); ?></h3>
+                    <?php echo the_excerpt(); ?>
+                    <a href="<?php echo the_permalink(); ?>" class="button button--lg">View article</a>
+                </div>
+            </div>
+
+        <?php
+            endwhile;
+            endif;
+
+            //Reset post data to main query one
+            wp_reset_postdata();
+        ?>
+            </div>
     </main>
     <?php get_template_part('template-parts/sidebar') ?>
 </div>

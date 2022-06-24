cb66e885d5f0a19d205311d4af5d7fb4c290f385
Xavier-lamy <xavier.lamy.pro@gmail.com>
2022-06-24 15:27:32 +0200

    Update functions.php to reflect webpack changes

--- wp-content/themes/armet-v2/inc/functions-config.php
+++ wp-content/themes/armet-v2/inc/functions-config.php
@@ -1,14 +1,15 @@
 <?php
 
 function armet_enqueue_assets() {
+    $version = 1.0;
     // Load style.css
     wp_enqueue_style( 'armet_style', get_stylesheet_uri(), array(), 1.0 );
   
     // Load CSS
-    wp_enqueue_style( 'armet_main', get_template_directory_uri() . '/assets/css/main.css', array(), 1.0 );
+    wp_enqueue_style( 'armet_main', get_template_directory_uri() . '/dist/style.css', array(), $version );
 
     // Load JS
-    wp_enqueue_script( 'armet_script', get_template_directory_uri() . '/assets/js/script.js', array(), 1.0, true );
+    wp_enqueue_script( 'armet_script', get_template_directory_uri() . '/dist/app.js', array(), $version, true );
 
     // Load Font Awesome library
     wp_enqueue_script( 'fa_kit', 'https://kit.fontawesome.com/0cb3a70ede.js', array(), 1.0, true );
